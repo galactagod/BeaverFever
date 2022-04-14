39a9be0e6d17bf46b33f6241effb15ed1a70be9e
Chandler Hale <cbeckerhale@knights.ucf.edu>
2022-04-14 19:08:58 -0400

    Fixed EventManager.cs

---
+++ src/Autoloads/EventManager.cs
@@ -0,0 +1,17 @@
+using Godot;
+using System;
+
+public class EventManager : Node
+{
+    Arraylist chestEventList = new Arraylist();
+
+    public override void _Ready()
+    {
+
+    }
+
+    public override void _Process(float delta)
+    {
+        
+    }
+}
---
+++ src/Objects/Chest/Chest.cs
@@ -0,0 +1,38 @@
+using Godot;
+using System;
+
+public class Chest: Node
+{
+    [Export] private int id = "A";
+
+    //id will always be the first item in the arraylist
+    private int idpos = 0;
+
+    //false if unopened
+    private bool opened;
+
+    //opened status will always be in the second position
+    private int openedpos = 1;
+
+    //Node reference
+    private EventManager _ndEventManager;
+    
+    public override void _Ready()
+    {
+        _ndEventManager = GetNode<EventManager>("/root/EventManager");
+
+        for(int i=0; i < _ndEventManager.chestEventList.Size; i++)
+        {
+            if(_ndEventManager.chestEventList[i][idpos] == id)
+            {
+                //opened status will always be in the second position
+                opened = _ndEventManager.chestEventList[i][openedpos];
+            }
+        }
+    }
+
+    public override void _Process(float delta)
+    {
+        
+    }
+}
