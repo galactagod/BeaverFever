ab52ca576bdf2d689ffa1fca9fb47756cb51d82a
int-trev <trevor.lar@gmail.com>
2022-04-24 19:48:16 -0400

    hi

--- src/Objects/Items/SignPost.cs
+++ src/Objects/Items/SignPost.cs
@@ -6,15 +6,22 @@ public class SignPost : Area2D
     [Export]
     public int whichDialogue;
 
+    DialogueManager dialogueManager;
+
     public override void _Ready()
     {
         this.Connect("body_entered", this, nameof(OnBodyEntered));
+        dialogueManager = GetNode<DialogueManager>("/root/DialogueManager");
     }
 
     public void OnBodyEntered(Node body)
     {
         if(body is ObjPlayer)
+        {
             Console.WriteLine("The message is " + whichDialogue);
+            dialogueManager.ShowDialogueElement();
+        }
+            
 
     }
 }
